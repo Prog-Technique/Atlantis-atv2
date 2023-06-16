bac11849a4be1b3d8765e23cf8d770fd6ee8fe04
amandavo <amandavoliveira0@gmail.com>
2023-06-16 19:24:45 -0300

    duas listagens novas

--- atlantis/src/ts/processos/Listagem/listagemTitulares.ts
+++ atlantis/src/ts/processos/Listagem/listagemTitulares.ts
@@ -1,8 +1,8 @@
-import Processo from "../abstracoes/processo";
-import Armazem from "../dominio/armazem";
-import ImpressorCliente from "../impressores/impressorCliente";
-import Impressor from "../interfaces/impressor";
-import Cliente from "../modelos/cliente";
+import Processo from "../../abstracoes/processo";
+import Armazem from "../../dominio/armazem";
+import ImpressorCliente from "../../impressores/impressorCliente";
+import Impressor from "../../interfaces/impressor";
+import Cliente from "../../modelos/cliente";
 
 export default class ListagemTitulares extends Processo {
     private clientes: Cliente[]
